dde007ae2ee18b4291cd3b02c80428deeb342360
Codezigineer <53050996+Codezigineer@users.noreply.github.com>
2020-06-24 21:56:30 -0700

    Update ui.h
    Make a new structure for widgets.

--- ui.h
+++ ui.h
@@ -6,7 +6,14 @@
 
 namespace ui
 {
-    extern int typeofwidget = 0; /* This Shows What Type Of Widget It Is. Here Is A List/Refrence:
+    using x = int; // The X Coord of a widget.
+    using y = int; // The Y Coord of a widget.
+    using width = int; // The Width of a widget.
+    using height = int; // The Height of a widget.
+    
+    struct widget 
+    {
+        extern int typeofwidget = 0; /* This Shows What Type Of Widget It Is. Here Is A List/Refrence:
     0 - The whole UI.
     1 - Some Text.
     2 - Some Buttons.
@@ -25,15 +32,23 @@ namespace ui
     15 - Custom UI.
     16 - Custom Widget.
     */
-    extern istream userinputwidget; // A Variable That You Extract Into, Then A Text Widget Pops up On The UI.
-    extern ostream textwidget; // Like userinputwdget, But Instead, Outputs Text.
-    extern istream searchbarwidget; // Like userinputwidget, But for Search-Only.
-    extern istream emailwidget; // Like userinputwidget, But for Email-Only.
+        widget children[]; // The children widgets.
+        widget parent; // The parent widget.
+        x x_coord; // The X Coord.
+        y y_coord; // The Y Coord.
+        width width; // The Width.
+        height height; // The Height.
+    };
+    
+    extern istream userinputwidgetstream; // A Variable That You Extract Into, Then A Text Widget Pops up On The UI.
+    extern ostream textwidgetstream; // Like userinputwdget, But Instead, Outputs Text.
+    extern istream searchbarwidgetstream; // Like userinputwidget, But for Search-Only.
+    extern istream emailwidgetstream; // Like userinputwidget, But for Email-Only.
     extern istream passwordwidget; // Like userinputwidget, But for Password-Only.
-    extern ostream headingwidget; // Like textwidget, But for Heading-Only.
-    extern ostream subheadingwidget; // Like textwidget, But for Subheading-Only.
+    extern ostream headingwidgetstream; // Like textwidget, But for Heading-Only.
+    extern ostream subheadingwidgetstream; // Like textwidget, But for Subheading-Only.
 }
 
-#define VERSION 1.0 # Here Is The Version.
+#define VERSION 1.0 // Here Is The Version.
 
 #endif
